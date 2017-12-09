d210f3a73448bc26410fe2117667dc853aca5382
iop4875127 <2433754916@qq.com>
2017-12-09 23:45:18 +0800

    tx demo @/xml

---
+++ spring_day03_tx/src/cn/itcast/service/TestService.java
@@ -0,0 +1,16 @@
+package cn.itcast.service;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestService {
+
+	@Test
+	public void testDemo() {
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("bean1.xml");
+		OrdersService service = (OrdersService) context.getBean("ordersService");
+		service.accountMoney();
+	}
+}
